1614385f4982235be5bcd4e628593b73e88cdc9b
David Hagen <david@drhagen.com>
2023-04-27 08:52:35 -0400

    Tests for default

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -152,6 +152,13 @@ def test_if_else():
     assert actual == expected
 
 
+def test_if_else_no_otherwise():
+    df = DataFrame(id=[0, 0, 0, 1, 1, 1], x=[2, 4, 5, 10, 11, -1])
+    actual = df.transmute(id="id", q="if_else(id == 1, x)")
+    expected = DataFrame(id=[0, 0, 0, 1, 1, 1], q=[None, None, None, 10, 11, -1])
+    assert actual == expected
+
+
 def test_if_else_grouped():
     df = DataFrame(id=[0, 0, 0, 1, 1, 1], x=[2, 5, 5, 10, 11, -13])
     actual = df.group_by("id").transmute(x="if_else(x == max(x), x, 0)")
@@ -159,6 +166,14 @@ def test_if_else_grouped():
     assert actual == expected
 
 
+def test_if_else_grouped_no_otherwise():
+    df = DataFrame(id=[0, 0, 0, 1, 1, 1], x=[2, 5, 5, 10, 11, -13])
+    actual = df.group_by("id").transmute(x="if_else(x == max(x), x)")
+    expected = DataFrame(id=[0, 0, 0, 1, 1, 1], x=[None, 5, 5, None, 11, None]).group_by("id")
+    assert actual == expected
+
+
+@pytest.mark.parametrize("default", ["", ", a"])
 @pytest.mark.parametrize(
     "df",
     [
@@ -171,7 +186,7 @@ def test_if_else_grouped():
         xfail_param(DataFrame(a=[], b=[], c=[]).group_by("a").group_by("b")),
     ],
 )
-def test_if_else_on_rowless_data_frame_with_mutate(df):
-    actual = df.mutate(x="if_else(a!=0, a, a)")
+def test_if_else_on_rowless_data_frame_with_mutate(default, df):
+    actual = df.mutate(x=f"if_else(a!=0, a{default})")
     expected = df.mutate(x="1")
     assert actual == expected
