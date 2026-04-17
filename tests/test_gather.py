c17071c27fdce33887132cd13a1383fce97ec660
David Hagen <david@drhagen.com>
2026-04-17 19:45:01 -0400

    Change gather to group by unmentioned columns (#73)
    * Gather groups on unmentioned columns rather than key
    * Expand spread and gather tests

--- tests/test_gather.py
+++ tests/test_gather.py
@@ -8,5 +8,59 @@ def test_gather():
         sex=["M", "F", "M", "F", "M", "F"],
         grades=["0", "0", "1", "1", "2", "2"],
         count=[8, 9, 9, 10, 6, 9],
-    ).group_by("grades")
+    ).group_by("sex")
+    assert actual == expected
+
+
+def test_gather_multiple_index_columns():
+    df = DataFrame.from_dict(
+        {"country": ["US", "US"], "sex": ["M", "F"], "0": [8, 9], "1": [9, 10]}
+    )
+    actual = df.gather("grades", "count", "0", "1")
+    expected = DataFrame(
+        country=["US", "US", "US", "US"],
+        sex=["M", "F", "M", "F"],
+        grades=["0", "0", "1", "1"],
+        count=[8, 9, 9, 10],
+    ).group_by("country", "sex")
+    assert actual == expected
+
+
+def test_gather_existing_group_levels():
+    df = DataFrame.from_dict(
+        {"country": ["US", "US"], "sex": ["M", "F"], "0": [8, 9], "1": [9, 10]}
+    ).group_by("country")
+    actual = df.gather("grades", "count", "0", "1")
+    expected = (
+        DataFrame(
+            country=["US", "US", "US", "US"],
+            sex=["M", "F", "M", "F"],
+            grades=["0", "0", "1", "1"],
+            count=[8, 9, 9, 10],
+        )
+        .group_by("country")
+        .group_by("sex")
+    )
+    assert actual == expected
+
+
+def test_gather_rowless():
+    df = DataFrame.from_dict({"sex": [], "0": [], "1": []})
+    actual = df.gather("grades", "count", "0", "1")
+    expected = DataFrame(sex=[], grades=[], count=[]).group_by("sex")
+    assert actual == expected
+
+
+def test_gather_empty_group_level():
+    df = DataFrame.from_dict({"sex": ["M", "F"], "0": [8, 9], "1": [9, 10]}).group_by()
+    actual = df.gather("grades", "count", "0", "1")
+    expected = (
+        DataFrame(
+            sex=["M", "F", "M", "F"],
+            grades=["0", "0", "1", "1"],
+            count=[8, 9, 9, 10],
+        )
+        .group_by()
+        .group_by("sex")
+    )
     assert actual == expected
--- tests/test_spread.py
+++ tests/test_spread.py
@@ -1,4 +1,7 @@
+import pytest
+
 from tabeline import DataFrame
+from tabeline.exceptions import NoGroupsError
 
 
 def test_spread():
@@ -7,4 +10,42 @@ def test_spread():
     )
     actual = df.group_by("sex").spread("grades", "count")
     expected = DataFrame.from_dict({"sex": ["M", "F"], "0": [8, 9], "1": [9, 10], "2": [6, 9]})
+    assert actual == expected
+
+
+def test_spread_multiple_group_levels():
+    df = DataFrame(
+        country=["US", "US", "US", "US"],
+        sex=["M", "F", "M", "F"],
+        grades=[0, 0, 1, 1],
+        count=[8, 9, 9, 10],
+    )
+    actual = df.group_by("country").group_by("sex").spread("grades", "count")
+    expected = DataFrame.from_dict(
+        {"country": ["US", "US"], "sex": ["M", "F"], "0": [8, 9], "1": [9, 10]}
+    ).group_by("country")
+    assert actual == expected
+
+
+def test_spread_no_groups():
+    df = DataFrame(grades=[0, 0, 1, 1], sex=["M", "F", "M", "F"], count=[8, 9, 9, 10])
+    with pytest.raises(NoGroupsError):
+        df.spread("grades", "count")
+
+
+def test_spread_rowless():
+    df = DataFrame(grades=[], sex=[], count=[]).group_by("sex")
+    actual = df.spread("grades", "count")
+    expected = DataFrame(sex=[])
+    assert actual == expected
+
+
+def test_spread_empty_group_level():
+    df = (
+        DataFrame(sex=["M", "F", "M", "F"], grades=[0, 0, 1, 1], count=[8, 9, 9, 10])
+        .group_by()
+        .group_by("sex")
+    )
+    actual = df.spread("grades", "count")
+    expected = DataFrame.from_dict({"sex": ["M", "F"], "0": [8, 9], "1": [9, 10]}).group_by()
     assert actual == expected
